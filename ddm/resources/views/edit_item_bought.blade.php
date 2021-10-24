7968d2b353737605557e9f7f9858879c9ef7d6b3
Info Qaswaweb <info@qaswaweb.com>
2021-10-24 19:27:01 +0500

    Edit item model changes
    Edit item model changes

--- ddm/resources/views/edit_item_bought.blade.php
+++ ddm/resources/views/edit_item_bought.blade.php
@@ -5,9 +5,21 @@
             <label class="form-label">Name</label>
             <input class="form-control" name="food-item" value="{{ $data['item']->name }}">
         </div>
+        @php
+         if(isset($data['item']->qty) and $data['item']->qty !=""){
+         $weight = $data['item']->qty;
+         }
+        else {
+                 $weight = $data['item']->weight;
+        }
+        @endphp
         <div class="mb-3">
             <label class="form-label">Weight</label>
-            <input class="form-control" name="food-item" value="{{ $data['item']->weight }}">
+            <input class="form-control" name="food-item" value="{{ $weight }}">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Quantity</label>
+            <input class="form-control" name="food-item" value=" {{ $data['item']->frequency }}">
         </div>
     </div>
     <div class="col-md-6">
@@ -16,11 +28,28 @@
             <input class="form-control" name="food-item" value="{{ $data['item']->price }}">
         </div>
         <div class="mb-3">
-            <label class="form-label">Quantity</label>
-            <input class="form-control" name="food-item" value=" {{ $data['item']->frequency }}">
+            <?php
+                if(isset($data['item']->qty) and $data['item']->qty !=""){
+                    $weight = $data['item']->qty;
+                    ?>
+                    <label class="form-label">Unit</label>
+                    <select id="unit" name="unit[]" class="form-select">
+                        <option value="pcs" selected>Pcs</option>
+                        <option value="g">g</option>
+                    </select>
+                <?php } else {
+                    $weight = $data['item']->weight;
+                    ?>
+                    <label class="form-label">Unit</label>
+                    <select id="unit" name="unit[]" class="form-select">
+                        <option value="pcs">Pcs</option>
+                        <option value="g" selected>g</option>
+                    </select>
+                <?php } ?>
         </div>
     </div>
     <input type="hidden" name="id" value="{{$data['id']}}">
+
     <div class="col-md-6">
         <label  class="form-label">Shop</label>
         <select name="shop" class="form-select">
